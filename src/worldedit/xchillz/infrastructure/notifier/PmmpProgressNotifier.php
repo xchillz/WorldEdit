dcc58c1f2de1dac36417128faba7af418c58f12f
xchillz <147117006+xchillz@users.noreply.github.com>
2026-08-03 20:36:48 -0400

    feat(infrastructure): add notifier module

---
+++ src/worldedit/xchillz/infrastructure/notifier/PmmpProgressNotifier.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace worldedit\xchillz\infrastructure\notifier;
+
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use worldedit\xchillz\application\port\ProgressNotifier;
+
+final class PmmpProgressNotifier implements ProgressNotifier
+{
+    /** @var Server */
+    private $server;
+
+    public function __construct(Server $server)
+    {
+        $this->server = $server;
+    }
+
+    public function notifyProgress(string $playerName, float $progress)
+    {
+        $player = $this->server->getPlayerExact($playerName);
+        if ($player === null || !$player->isOnline()) {
+            return;
+        }
+
+        $player->sendPopup(
+            TextFormat::GREEN .
+                ": " .
+                TextFormat::WHITE .
+                round($progress * 100) .
+                "%"
+        );
+    }
+
+    public function notifyComplete(string $playerName, int $totalBlocks)
+    {
+        $player = $this->server->getPlayerExact($playerName);
+        if ($player === null || !$player->isOnline()) {
+            return;
+        }
+
+        $player->sendMessage(
+            TextFormat::GREEN .
+                "Operation completed: " .
+                TextFormat::WHITE .
+                $totalBlocks .
+                TextFormat::GREEN .
+                " blocks."
+        );
+    }
+}
