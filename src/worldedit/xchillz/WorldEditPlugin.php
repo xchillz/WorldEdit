6bb52e333f3d06d1085e4a75959f8ff810035f73
xchillz <147117006+xchillz@users.noreply.github.com>
2026-08-03 21:34:57 -0400

    feat: wire all features to pluginbase

--- src/worldedit/xchillz/WorldEditPlugin.php
+++ src/worldedit/xchillz/WorldEditPlugin.php
@@ -5,5 +5,62 @@ declare(strict_types=1);
 namespace worldedit\xchillz;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use worldedit\xchillz\application\service\BatchExecutionService;
+use worldedit\xchillz\application\usecase\CopyUseCase;
+use worldedit\xchillz\application\usecase\PasteUseCase;
+use worldedit\xchillz\application\usecase\SetBlocksUseCase;
+use worldedit\xchillz\application\usecase\UndoUseCase;
+use worldedit\xchillz\infrastructure\notifier\PmmpProgressNotifier;
+use worldedit\xchillz\infrastructure\repository\InMemorySessionRepository;
+use worldedit\xchillz\infrastructure\scheduler\PmmpTaskScheduler;
+use worldedit\xchillz\infrastructure\world\PmmpLevelAdapter;
+use worldedit\xchillz\presentation\command\WorldEditCommand;
+use worldedit\xchillz\presentation\listener\WandInteractListener;
 
-final class WorldEditPlugin extends PluginBase {}
+final class WorldEditPlugin extends PluginBase
+{
+    public function onEnable()
+    {
+        $sessionRepository = new InMemorySessionRepository();
+
+        $worldAdapter = new PmmpLevelAdapter($this->getServer());
+        $notifier = new PmmpProgressNotifier($this->getServer());
+
+        $scheduler = new PmmpTaskScheduler($this, $worldAdapter, $notifier);
+
+        $batchExecutionService = new BatchExecutionService($scheduler);
+
+        $setBlocksUseCase = new SetBlocksUseCase($sessionRepository, $worldAdapter, $batchExecutionService);
+        $copyUseCase = new CopyUseCase($sessionRepository, $worldAdapter);
+        $pasteUseCase = new PasteUseCase($sessionRepository, $batchExecutionService);
+        $undoUseCase = new UndoUseCase($sessionRepository, $batchExecutionService);
+
+        $worldEditCommand = new WorldEditCommand(
+            $sessionRepository,
+            $setBlocksUseCase,
+            $copyUseCase,
+            $pasteUseCase,
+            $undoUseCase
+        );
+        $this->getServer()->getCommandMap()->register("worldedit", $worldEditCommand);
+
+        $this->getServer()->getPluginManager()->registerEvents(
+            new WandInteractListener($sessionRepository),
+            $this
+        );
+
+        $this->getLogger()->info(
+            TextFormat::GREEN .
+                "World Edit plugin loaded just fine!"
+        );
+    }
+
+    public function onDisable()
+    {
+        $this->getLogger()->info(
+            TextFormat::RED .
+                "World Edit plugin is now disabled."
+        );
+    }
+}
